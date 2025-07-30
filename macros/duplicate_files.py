8b6acc430403e5c1149404227aebb6f9f486e7bb
Andrew Brinkerhoff <abrinke1@cern.ch>
2025-07-31 00:08:05 +0200

    Update duplicate_files.py for 2025_07_25

--- macros/duplicate_files.py
+++ macros/duplicate_files.py
@@ -4,8 +4,9 @@
 import os
 import ROOT as R
 
-IN_DIR_A = '/eos/cms/store/user/abrinke1/HiggsToAA/2DAlphabet/raw_inputs/2025_07_14/'
-IN_DIR_B = '/eos/cms/store/user/abrinke1/HiggsToAA/2DAlphabet/plots/2025_07_14/'
+IN_DIR_A = '/eos/cms/store/user/abrinke1/HiggsToAA/2DAlphabet/raw_inputs/2025_07_25/'
+#IN_DIR_B = '/eos/cms/store/user/abrinke1/HiggsToAA/2DAlphabet/plots/2025_07_25/'
+
 YEAR_A = '2016preVFP'
 YEAR_B = '2016postVFP'
 SAMP = 'VBFHtoaato4b_mA_55'
@@ -18,11 +19,12 @@ def main():
     print('to avoid "list is accessing an object already deleted" error! - AWB 2024.06.24\n')
     print('See https://root-forum.cern.ch/t/error-in-tlist-clear-a-list-is-accessing-an-object-already-deleted-list-name-tlist-when-opening-a-file-created-by-root-6-30-using-root-6-14-09/57588/1')
 
-    for top_dir in [IN_DIR_A, IN_DIR_B]:
+    #for top_dir in [IN_DIR_A, IN_DIR_B]:
+    for top_dir in [IN_DIR_A]:
         print('\n\n*** Duplicating files in %s ***' % top_dir)
         for o_dir in [top_dir+sub_dir for sub_dir in os.listdir(top_dir)]:
-            if '/Zll' in o_dir or '/Wlv' in o_dir or '/ttb' in o_dir:
-                continue  ## Skip leptonic directories
+            if not '/VBFjj' in o_dir and not '/gg0l' in o_dir and not '/Vjj' in o_dir and not '/tt0l' in o_dir and not '/Zvv' in o_dir:
+                continue  ## Only applies to hadronic categories
             in_dir_A = o_dir+'/'+YEAR_A+'/'
             f_list_A = [in_dir_A+fl for fl in os.listdir(in_dir_A) if SAMP in fl]
             for fn_A in f_list_A:
