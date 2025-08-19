de59da208278c270daf55c466019131b8d7c06ae
Andrew Brinkerhoff <abrinke1@cern.ch>
2025-08-19 17:19:49 +0200

    Update fetchtoyinput for 2025_08_15

--- fetchtoyinput.sh
+++ fetchtoyinput.sh
@@ -2,50 +2,74 @@
 
 #######################################################
 # Fetch input ROOT files to be used in mergetoyinput.sh
-# Takes ~5 minutes (not including VBFjj)               
+# Takes 5 - 10 minutes
 #######################################################
 
-DATE="2025_07_25"
+DATE="2025_08_15"
 source config/user.config  ## Loads USER, LOC_DIR, and EOS_DIR
 OUTDIR="${EOS_DIR}/raw_inputs/${DATE}"
+
+HADDIR="/eos/cms/store/user/ssawant/htoaa/analysis/20250811_DatacardsFullSyst"
+VBFDIR="/afs/cern.ch/work/m/moanwar/public/hto2ato4b/newVBFCat/2DAlphabet_inputFiles"
+LEPDIR="/eos/user/h/hboucham/public/2D_Alphabet_Inputs"
+LEPTAG="081425"
+
+DOHAD=false
+DOVBF=true
+DOLEP=false
 
 # Start the timer
 START_TIME=$SECONDS
 
-# Emptying the input directory
-echo " > Make new directory ${OUTDIR}"
-if [ -d $OUTDIR ]; then
-    rm -rf $OUTDIR
-fi
-mkdir -p $OUTDIR
+# # Emptying the input directory
+# echo " > Make new directory ${OUTDIR}"
+# if [ -d $OUTDIR ]; then
+#     rm -rf $OUTDIR
+# fi
+# mkdir -p $OUTDIR
 
 # Copy all 2D plots
 echo " > Copy all input 2D histogram files to raw_inputs ..."
 # -- Hadronic categories from Siddhesh (gg0l, Vjj, tt0l, Zvv) --
 echo "Starting hadronic ..."
-for CAT in gg0lLo gg0lHi VjjLo VjjHi tt0l0b tt0l1b ZvvLo ZvvHi; do
+for CAT in gg0lLo gg0lHi VjjLo VjjHi VjjLo350 VjjHi350 VjjLo400 VjjHi400 tt0l0b tt0l1b ZvvLo ZvvHi; do
+    if [ "$DOHAD" = false ]; then
+	echo "... actually, skipping!"
+	break
+    fi
     mkdir -p ${OUTDIR}/${CAT}/Run2
+    rm -r ${OUTDIR}/${CAT}/Run2/*
     for YEAR in 2016preVFP 2016postVFP 2017 2018; do
 	mkdir -p ${OUTDIR}/${CAT}/${YEAR}
-	echo "cp -r /eos/cms/store/user/ssawant/htoaa/analysis/20250722_DatacardsFullSyst/${YEAR}/*/2DAlphabet_inputFiles/${CAT}/*root ${OUTDIR}/${CAT}/${YEAR}/"
-	cp -r /eos/cms/store/user/ssawant/htoaa/analysis/20250722_DatacardsFullSyst/${YEAR}/*/2DAlphabet_inputFiles/${CAT}/*root ${OUTDIR}/${CAT}/${YEAR}/
+	rm -r ${OUTDIR}/${CAT}/${YEAR}/*
+	echo "cp -r ${HADDIR}/${YEAR}/*/2DAlphabet_inputFiles/${CAT}/*root ${OUTDIR}/${CAT}/${YEAR}/"
+	cp -r ${HADDIR}/${YEAR}/*/2DAlphabet_inputFiles/${CAT}/*root ${OUTDIR}/${CAT}/${YEAR}/
     done
 done
 
 # -- VBF --
 echo "Starting VBF ..."
-for CAT in VBFjjLo VBFjjHi; do
+declare -A CATS
+CATS["VBFjjLo"]="VBFLo"
+CATS["VBFjjHi"]="VBFHi"
+CATS["VBFjjLoPtLo"]="VBFLoPTLo"
+CATS["VBFjjLoPtHi"]="VBFLoPTHi"
+CATS["VBFjjHiPtLo"]="VBFHiPTLo"
+CATS["VBFjjHiPtHi"]="VBFHiPTHi"
+for CAT in ${!CATS[@]}; do
+    if [ "$DOVBF" = false ]; then
+	echo "... actually, skipping!"
+	break
+    fi
     mkdir -p ${OUTDIR}/${CAT}/Run2
+    rm -r ${OUTDIR}/${CAT}/Run2/*
     for YEAR in 2016preVFP 2016postVFP 2017 2018; do
+	CATIN="${CATS[${CAT}]}"
+	INDIR="${VBFDIR}/${YEAR}/VBFjj/2DAlphabet_inputFiles/${CATIN}"
 	mkdir -p ${OUTDIR}/${CAT}/${YEAR}
-	CATIN="${CAT}"
-	if [[ "${CAT}" == "VBFjjLo" ]]; then
-		CATIN="VBFLo"
-	elif [[ "${CAT}" == "VBFjjHi" ]]; then
-		CATIN="VBFHi"
-	fi
-	echo "cp -r /afs/cern.ch/work/m/moanwar/public/hto2ato4b/2DAlphabetfiles_VBF_sys/20250721_DataMC/${YEAR}/VBFjj/2DAlphabet_inputFiles/${CATIN}/*root ${OUTDIR}/${CAT}/${YEAR}/"
-	cp -r /afs/cern.ch/work/m/moanwar/public/hto2ato4b/2DAlphabetfiles_VBF_sys/20250721_DataMC/${YEAR}/VBFjj/2DAlphabet_inputFiles/${CATIN}/*root ${OUTDIR}/${CAT}/${YEAR}/
+	rm -r ${OUTDIR}/${CAT}/${YEAR}/*
+	echo "cp -r ${INDIR}/*root ${OUTDIR}/${CAT}/${YEAR}/"
+	cp -r ${INDIR}/*root ${OUTDIR}/${CAT}/${YEAR}/
 	echo "Changing file names from ${CATIN} to ${CAT}"
 	cd ${OUTDIR}/${CAT}/${YEAR}/
 	for FILEIN in *root; do
@@ -60,18 +84,24 @@ done
 # -- Leptonic categories from Hichem (Zll, Wlv, ttlv, ttll) --
 echo "Starting leptonic ..."
 for CAT in Zll WlvLo WlvHi ttblv ttbblv ttbll; do
+    if [ "$DOLEP" = false ]; then
+	echo "... actually, skipping!"
+	break
+    fi
     for WP in WP60; do
 	mkdir -p ${OUTDIR}/${CAT}/${WP}/Run2
+	rm -r ${OUTDIR}/${CAT}/${WP}/Run2/*
 	for YEAR in 2016preVFP 2016postVFP 2017 2018; do
 	    mkdir -p ${OUTDIR}/${CAT}/${WP}/${YEAR}
+	    rm -r ${OUTDIR}/${CAT}/${WP}/${YEAR}/*
 	    YEARIN=${YEAR:2:2}  ## Last 2 digits of  year (16, 17, 18)
 	    if [[ "${YEAR}" == "2016preVFP" ]]; then
 		YEARIN="16APV"
 	    elif [[ "${YEAR}" == "2016postVFP" ]]; then
 		YEARIN="16"
 	    fi
-	    echo "cp /eos/user/h/hboucham/public/2D_Alphabet_Inputs/2D${YEARIN}_*_072225/${WP}/${CAT}_*root ${OUTDIR}/${CAT}/${WP}/${YEAR}/"
-	    cp /eos/user/h/hboucham/public/2D_Alphabet_Inputs/2D${YEARIN}_*_072225/${WP}/${CAT}_*root ${OUTDIR}/${CAT}/${WP}/${YEAR}/
+	    echo "cp ${LEPDIR}/2D${YEARIN}_*_${LEPTAG}/${WP}/${CAT}_*root ${OUTDIR}/${CAT}/${WP}/${YEAR}/"
+	    cp ${LEPDIR}/2D${YEARIN}_*_${LEPTAG}/${WP}/${CAT}_*root ${OUTDIR}/${CAT}/${WP}/${YEAR}/
 	done
     done
 done
