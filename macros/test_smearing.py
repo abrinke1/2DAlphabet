dd895ec4ef37aab5db12033f643d4b422f359c05
Andrew Brinkerhoff <abrinke1@cern.ch>
2025-09-25 17:35:44 +0200

    test_smearing for mass(a)

--- macros/test_smearing.py
+++ macros/test_smearing.py
@@ -1,15 +1,22 @@
 import numpy as np
 import statistics as stat
-nevt = 10000000  ## Number of random events thrown for each smearing
+nevt = 1000000  ## Number of random events thrown for each smearing
 #for smr in [0.03+0.01*ii for ii in range(3)]:  ## Smearing factor
-for smr in [0.04]:  ## Smearing factor
+#for smr in [0.07]:  ## Smearing factor
+for smr in [0.20]:  ## Smearing factor
     #for mass in [125.0, 175.0]:      ## Resonance peak (GeV)
     #    for wid in [15.0, 20.0, 25.0]:  ## Resonance width (GeV)
-    for mass in [125.0]:      ## Resonance peak (GeV)
-        for wid in [125.0*14.0/172.0, 14.0]:  ## Resonance width (GeV)
-        #for wid in [20.0]:  ## Resonance width (GeV)
+    # for mass in [125.0]:      ## Resonance peak (GeV)
+    #     for wid in [125.0*14.0/172.0, 14.0]:  ## Resonance width (GeV)
+    #         smr_sc = 0.023
+    # for mass in [125.0]:
+    #     for wid in [10.0]:  ## PNet regressed m(H) width
+    #         smr_sc = 0.03   ## Smearing factor to increase width by 7%
+    for mass in [15.0,30.0,55.0]:
+        for wid in [mass*0.068]:  ## PNet regressed m(a) width
+            smr_sc = 0.045        ## Smearing factor to increase width by 20%
+
             #smr_sc = np.sqrt(2.0*smr)*wid/mass
-            smr_sc = 0.023
             nom = np.random.normal(mass, wid, nevt)
             nom_med = stat.median(nom)
             nom_mn = stat.mean(nom)
